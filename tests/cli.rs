0ad01a1af2e04c26396e8c3b534c69bb0eee4cc6
Reilly <reilly.wood@icloud.com>
2019-10-05 10:20:16 -0700

    Add more basic integration tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -5,3 +5,18 @@ use assert_cmd::prelude::*;
 fn fail_if_no_params_provided() {
     Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap().assert().failure();
 }
+
+#[test]
+fn fail_if_file_not_found() {
+    Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap()
+    .args(&["-t","file_that_doesnt_exist.tar"])
+    .assert().failure();
+}
+
+
+#[test]
+fn succeed_if_file_found() {
+    Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap()
+    .args(&["-t","test-files/single-text-file.tar"])
+    .assert().success();
+}
