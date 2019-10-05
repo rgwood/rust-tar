4d4a0470e8db011ffc9fe4592f2fe01a38943a20
Reilly <reilly.wood@icloud.com>
2019-10-05 09:48:23 -0700

    Move parsing logic into Header struct

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+// Because this one is super annoying during development
+#![allow(dead_code)]
 use std::fs::File;
 use std::io::prelude::*;
 use std::str;
 use structopt::StructOpt;
 use tarlib::*;
 use std::fs::OpenOptions;
-// use std::path::Path;
 
 #[derive(StructOpt)]
 struct CliParams {
@@ -17,6 +18,41 @@ struct CliParams {
     /// The path to a tarball
     #[structopt(required_if("extract_mode", "true"))]
     file_name: String
+}
+
+struct Header<'a> {
+    bytes: &'a[u8],
+    file_name: &'a str,
+    checksum: usize,
+    file_size_in_bytes: usize
+}
+
+impl Header<'_> {
+
+    fn calculate_checksum(&self) -> usize {
+        let mut calculated_checksum: usize = 0;
+        for b in &self.bytes[..148] {
+            calculated_checksum += usize::from(*b) ;
+        }   
+
+        // checksum bytes are taken to be ASCII spaces (decimal value 32)
+        calculated_checksum += 8 * 32;
+
+        for b in &self.bytes[156..512] {
+            calculated_checksum += usize::from(*b);
+        }   
+
+        calculated_checksum
+    }
+}
+
+fn parse_header(bytes: &[u8]) -> Header {
+    Header 
+    {bytes, 
+    file_name: bytes_to_str(truncate_null_terminated_seq(&bytes[..99])),
+    checksum: convert_octal_to_decimal(&bytes[148..154]),
+    file_size_in_bytes: convert_octal_to_decimal(&bytes[124..135])
+    }
 }
 
 const TAR_HEADER_LENGTH_IN_BYTES: usize = 512;
@@ -34,28 +70,26 @@ fn main() -> std::io::Result<()> {
 
     if args.list_mode || args.extract_mode {
         let tarball_filename = &args.file_name;
-        let mut tarball = File::open(tarball_filename).expect("Failed to open file");;
+        let mut tarball = File::open(tarball_filename).expect("Failed to open file");
 
         // TODO: do something smarter than reading the entire file into memory, maybe a BufReader
         let mut tarball_contents: Vec<u8> = Vec::new();
         let file_size_in_bytes = tarball.read_to_end(&mut tarball_contents)?;
         println!("Read all {} bytes from file '{}' successfully ", file_size_in_bytes, tarball_filename);
 
-        let tar_header: &[u8] = &tarball_contents[..TAR_HEADER_LENGTH_IN_BYTES];
+        let header = parse_header(&tarball_contents[..TAR_HEADER_LENGTH_IN_BYTES]);
 
-        let file_name_from_header = bytes_to_str(truncate_null_terminated_seq(&tar_header[..99]));
-
-        println!("File name from tarball: '{}'", file_name_from_header);
+        println!("File name from tarball: '{}'", header.file_name);
 
         // TODO: verify checksum.
-        println!("Decimal checksum: {}", convert_octal_to_decimal(&tar_header[148..154]));
-        println!("Calculated checksum: {}", calculate_header_checksum(tar_header));
+        println!("Decimal checksum: {}", header.checksum);
+        println!("Calculated checksum: {}", header.calculate_checksum());
 
-        let file_size_bytes = convert_octal_to_decimal(&tar_header[124..135]);
+        // let file_size_bytes = convert_octal_to_decimal(&tar_header[124..135]);
 
-        println!("File size in bytes: {}", file_size_bytes);
+        println!("File size in bytes: {}", header.file_size_in_bytes);
 
-        let file_contents = &tarball_contents[TAR_HEADER_LENGTH_IN_BYTES..TAR_HEADER_LENGTH_IN_BYTES+file_size_bytes];
+        let file_contents = &tarball_contents[TAR_HEADER_LENGTH_IN_BYTES..TAR_HEADER_LENGTH_IN_BYTES+header.file_size_in_bytes];
 
         println!("File contents: {}", bytes_to_str(file_contents));
 
@@ -64,9 +98,9 @@ fn main() -> std::io::Result<()> {
             let mut extracted_file = OpenOptions::new()
                             .write(true)
                             .create_new(true)
-                            .open(file_name_from_header)?;
+                            .open(header.file_name)?;
             extracted_file.write_all(file_contents)?;
-            println!("Extracted file {} successfully", file_name_from_header);
+            println!("Extracted file {} successfully", header.file_name);
         }
 
     }
@@ -84,22 +118,6 @@ fn convert_octal_to_decimal(slice: &[u8]) -> usize {
             panic!("Could not parse octal checksum")
             }
     }
-}
-
-fn calculate_header_checksum(header: &[u8]) -> u32 {
-    let mut calculated_checksum: u32 = 0;
-    for b in &header[..148] {
-        calculated_checksum += u32::from(*b) ;
-    }
-
-    // checksum bytes are taken to be ASCII spaces (decimal value 32)
-    calculated_checksum += 8 * 32;
-    
-    for b in &header[156..512] {
-        calculated_checksum += u32::from(*b);
-    }
-
-    calculated_checksum
 }
 
 fn bytes_to_str(bytes: &[u8]) -> &str {
